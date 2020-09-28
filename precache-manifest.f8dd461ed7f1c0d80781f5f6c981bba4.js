65fa4e8129105e58f06408e83387076bc4bb550c
Alex <jalexgould@gmail.com>
2020-09-28 12:52:39 -0400

    Updates

---
+++ precache-manifest.f8dd461ed7f1c0d80781f5f6c981bba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24256d0946f801d2e5670f0372af84cc",
+    "url": "/jalexgould/deploy-test.git/index.html"
+  },
+  {
+    "revision": "77aa43fdd3d07b05cf41",
+    "url": "/jalexgould/deploy-test.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "163782385e5f88e0b5be",
+    "url": "/jalexgould/deploy-test.git/static/js/2.6bb47cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jalexgould/deploy-test.git/static/js/2.6bb47cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77aa43fdd3d07b05cf41",
+    "url": "/jalexgould/deploy-test.git/static/js/main.94895520.chunk.js"
+  },
+  {
+    "revision": "6f2df98a5d64ae7822b8",
+    "url": "/jalexgould/deploy-test.git/static/js/runtime-main.701d8d68.js"
+  }
+]);
